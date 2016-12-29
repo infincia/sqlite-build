58a6ce97489e6912e9e7320d806b406b9ffc27d1
Stephen Oliver <steve@infincia.com>
2016-12-29 16:09:43 -0500

    fix invalid compiler command

--- release.cmd
+++ release.cmd
@@ -5,11 +5,11 @@ mkdir dist-$TARGET\lib\MT
 mkdir dist-$TARGET\lib\MD
 mkdir dist-$TARGET\include
 
-cl /c /EHsc /MT sqlite3\sqlite3.c /OUT:sqlite3MT.obj
-lib sqlite3MT.obj /MACHINE:%ARCH% /OUT:dist-$TARGET\lib\MT\sqlite3.lib
+cl /c /EHsc /MT sqlite3\sqlite3.c
+lib sqlite3.obj /MACHINE:%ARCH% /OUT:dist-$TARGET\lib\MT\sqlite3.lib
 
-cl /c /EHsc /MD sqlite3\sqlite3.c /OUT:sqlite3MD.obj
-lib sqlite3MD.obj /MACHINE:%ARCH% /OUT:dist-$TARGET\lib\MD\sqlite3.lib
+cl /c /EHsc /MD sqlite3\sqlite3.c
+lib sqlite3.obj /MACHINE:%ARCH% /OUT:dist-$TARGET\lib\MD\sqlite3.lib
 
 copy sqlite3\sqlite3.h dist-$TARGET\include\
 copy sqlite3\sqlite3ext.h dist-$TARGET\include\
