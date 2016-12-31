9874e4985ded95774ad9214252ac82cc147e424b
Stephen Oliver <steve@infincia.com>
2016-12-30 23:35:21 -0500

    build update

--- release.cmd
+++ release.cmd
@@ -1,15 +1,18 @@
 ECHO building sqlite3 for Windows (%TARGET%)
 
 mkdir dist-%TARGET%
+mkdir dist-%TARGET%\lib
 mkdir dist-%TARGET%\lib\MT
 mkdir dist-%TARGET%\lib\MD
 mkdir dist-%TARGET%\include
 
-cl /c /EHsc /MT sqlite3\sqlite3.c
-lib sqlite3.obj /MACHINE:%ARCH% /OUT:dist-%TARGET%\lib\MT\sqlite3.lib
+cl /c /Gy /MT sqlite3\sqlite3.c
+lib /out:dist-%TARGET%\lib\MT\sqlite3.lib sqlite3.obj
 
-cl /c /EHsc /MD sqlite3\sqlite3.c
-lib sqlite3.obj /MACHINE:%ARCH% /OUT:dist-%TARGET%\lib\MD\sqlite3.lib
+cl /c /Gy /MD sqlite3\sqlite3.c
+lib /out:dist-%TARGET%\lib\MD\sqlite3.lib sqlite3.obj
+
+cl sqlite3\sqlite3.c -DSQLITE_API=__declspec(dllexport) -link -dll -out:dist-%TARGET%\lib\sqlite3.dll
 
 copy sqlite3\sqlite3.h dist-%TARGET%\include\
 copy sqlite3\sqlite3ext.h dist-%TARGET%\include\
